761fa404448fca54f2f53602682aea47cfc4760a
ZealousProxy <dan.mccarthy89@gmail.com>
2025-04-10 12:38:21 +1000

    Got it moving

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import*
+from player import*
 
 
 def main():
@@ -9,12 +10,21 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+
+	clock = pygame.time.Clock()
+	dt = 0
+	player =  Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+	
 	while 1 == 1:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
 		        	return
 		screen.fill((0,0,0))
+		player.update(dt)
+		player.draw(screen)
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.tick(60)
 
 if __name__ == "__main__":
 	main()
